955c8afc2fffd8d9b1d4f388af57c44af5d68197
Samagra-Singh <168257421+Sam-xiexielaoshi@users.noreply.github.com>
2024-08-18 23:04:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const http = require("http");
 const url = require("url");
 const replaceTemplate=require("./modules/replaceTemplate");
 
-//SERVERS
-
 const tempOverview = fs.readFileSync(
   `${__dirname}/templates/template-overview.html`,
   "utf-8"
@@ -60,33 +58,5 @@ server.listen(8000, "127.0.0.1", () => {
   console.log("listening to requests on port 8000");
 });
 
-////////////////////////////////////////////////////////////
-//FILES
 
-//blocking, synchornous way
-
-// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
-// console.log(textIn);
-
-// const textOut = `This is what we know about the avacado: ${textIn}.\nCreated on ${Date.now()}`;
-// fs.writeFileSync('./txt/output.txt', textOut);
-// console.log('File Written!');
-
-/////////////////////////////////////////////////////////////
-
-//non-blocking,  asynchronous way
-
-// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-//   if(err) return console.log("Error");
-//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-//     console.log(data2);
-//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", (err) => {
-//         console.log("filw written");
-//       });
-//     });
-//   });
-// });
-
-///////////////////////////////////////////////////////////////////
 
